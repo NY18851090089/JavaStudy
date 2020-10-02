bf58fec1df475601bfc9cc3f7594b7ab8f5bf532
NY18851090089 <71701237+NY18851090089@users.noreply.github.com>
2020-10-02 22:02:27 +0800

    10.2

---
+++ Test2_2.java
@@ -0,0 +1,10 @@
+public class Test2_2 {
+    public static void main(String[] args) {
+        int x =8;
+        byte b =127;
+        b = (byte)x;
+        x = 'a';
+        long y = b;
+        float z =(int)6.89;
+    }
+}
