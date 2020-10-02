c2096a194484c6801da61bba70024d393f89dff9
NY18851090089 <71701237+NY18851090089@users.noreply.github.com>
2020-10-02 21:10:16 +0800

    10.2
    枚举类型

---
+++ Example2_9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+enum Weekday{
+    xingqiyi,xingqier,xingqisan,xingqisi,xingqiwu,xingqiliu,xingiri
+}
+public class Example2_9 {
+    public static void main(String[] args) {
+        Weekday x= Weekday.xingiri;
+        if(x==Weekday.xingiri){
+            System.out.println(x);
+            System.out.println("jintianwoxiuxi");
+        }
+    }
+}
