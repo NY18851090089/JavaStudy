cc42e741f29a348f45d09cc58f597dc4c0cf865e
NY18851090089 <71701237+NY18851090089@users.noreply.github.com>
2020-10-03 15:04:39 +0800

    10.3

---
+++ Text2_6.java
@@ -0,0 +1,9 @@
+public class Text2_6{
+    public static void main(String args[])
+    {
+        char A='你',B='我',C='他';
+        System.out.println("汉字bai\'你'在unicode表中的du位置zhi："+(int)A);
+        System.out.println("汉字\'我'在unicode表中的位置："+(int)B);
+        System.out.println("汉字\'他'在unicode表中的位置："+(int)C);
+    }
+}
